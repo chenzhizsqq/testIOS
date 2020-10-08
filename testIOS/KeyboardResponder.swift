8175044c305bfa4da72d71b742c58944b117544b
chenzhizs <chenzhi@qq.com>
2020-10-08 12:10:57 +0900

    Add files via upload

---
+++ testIOS/KeyboardResponder.swift
@@ -0,0 +1,30 @@
+//
+//  KeyboardResponder.swift
+//  testIOS
+//
+//  Created by chenzhizs on 2020/10/08.
+//  Copyright © 2020 chenzhizs. All rights reserved.
+//
+
+import SwiftUI
+
+class KeyboardResponder: ObservableObject {
+    @Published var keyboardHeight: CGFloat = 0
+    
+    var keyboardShow: Bool { keyboardHeight > 0 }
+    
+    init() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIWindow.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIWindow.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let frame = notification.userInfo?[UIWindow.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        keyboardHeight = frame.height
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        keyboardHeight = 0
+    }
+}
