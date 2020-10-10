dd7f7538d59d362295115f49b1acacc75d50ff1b
chenzhizs <chenzhi@qq.com>
2020-10-10 13:00:44 +0900

    Update Post.swift

--- testIOS/Post.swift
+++ testIOS/Post.swift
@@ -1,12 +1,14 @@
 //
 //  Post.swift
-//  NetworkChen
+//  PostDemo
 //
-//  Created by chenzhizs on 2020/10/10.
-//  Copyright © 2020 chenzhizs. All rights reserved.
+//  Created by xiaoyouxinqing on 1/6/20.
+//  Copyright © 2020 xiaoyouxinqing. All rights reserved.
 //
 
 import Foundation
+import UIKit
+import SwiftUI
 
 struct PostList: Codable {
     var list: [Post]
@@ -27,4 +29,39 @@ struct Post: Codable, Identifiable {
     var commentCount: Int
     var likeCount: Int
     var isLiked: Bool
+}
+
+extension Post {
+    var avatarImage: Image {
+        return loadImage(name: avatar)
+    }
+    
+    var commentCountText: String {
+        if commentCount <= 0 { return "评论" }
+        if commentCount < 1000 { return "\(commentCount)" }
+        return String(format: "%.1fK", Double(commentCount) / 1000)
+    }
+    
+    var likeCountText: String {
+        if likeCount <= 0 { return "点赞" }
+        if likeCount < 1000 { return "\(likeCount)" }
+        return String(format: "%.1fK", Double(likeCount) / 1000)
+    }
+}
+
+func loadPostListData(_ fileName: String) -> PostList {
+    guard let url = Bundle.main.url(forResource: fileName, withExtension: nil) else {
+        fatalError("Can not find \(fileName) in main bundle")
+    }
+    guard let data = try? Data(contentsOf: url) else {
+        fatalError("Can not load \(url)")
+    }
+    guard let list = try? JSONDecoder().decode(PostList.self, from: data) else {
+        fatalError("Can not parse post list json data")
+    }
+    return list
+}
+
+func loadImage(name: String) -> Image {
+    return Image(uiImage: UIImage(named: name)!)
 }
