d92b7ecd1659a051de8078d21259cc9e21244a03
chenzhizs <chenzhi@qq.com>
2020-10-10 10:24:37 +0900

    update

---
+++ NetworkChen/NetworkChen/Post.swift
@@ -0,0 +1,21 @@
+//
+//  Post.swift
+//  NetworkChen
+//
+//  Created by chenzhizs on 2020/10/10.
+//  Copyright © 2020 chenzhizs. All rights reserved.
+//
+
+import SwiftUI
+
+struct Post: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Post_Previews: PreviewProvider {
+    static var previews: some View {
+        Post()
+    }
+}
