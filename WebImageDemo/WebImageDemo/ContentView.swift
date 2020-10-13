25f54ffb01beab9caaa000ff2fef10850968eab3
chenzhizs <chenzhi@qq.com>
2020-10-13 16:46:04 +0900

    Lesson 13 - 2
    BBCo - iOS开发入门教程 加载网络图片 Lesson 13 - 2 SDWebImageSwiftUI

---
+++ WebImageDemo/WebImageDemo/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  WebImageDemo
+//
+//  Created by chenzhizs on 2020/10/13.
+//  Copyright © 2020 chenzhizs. All rights reserved.
+//
+
+import SwiftUI
+
+private let url = URL(string: "https://raw.githubusercontent.com/chenzhizsqq/testIOS/master/testIOS/Resources/006PdkDogy1gap6ngj12ej30u011itf7.jpg")!
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List{
+                NavigationLink(destination: SimpleExample(url: url)) {
+                    Text("SimpleExample")
+                }
+                NavigationLink(destination: WebImageExample()) {
+                    Text("WebImageExample")
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebImageDemo/WebImageDemo/WebImageExample.swift
@@ -0,0 +1,21 @@
+//
+//  WebImageExample.swift
+//  WebImageDemo
+//
+//  Created by chenzhizs on 2020/10/13.
+//  Copyright © 2020 chenzhizs. All rights reserved.
+//
+
+import SwiftUI
+
+struct WebImageExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WebImageExample_Previews: PreviewProvider {
+    static var previews: some View {
+        WebImageExample()
+    }
+}
