2e1529c6232cd2ce5bbf251afc5886dc0274e00c
chenzhizs <chenzhi@qq.com>
2020-10-13 15:46:47 +0900

    WebImageDemo
    create

---
+++ WebImageDemo/WebImageDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebImageDemo
+//
+//  Created by chenzhizs on 2020/10/13.
+//  Copyright © 2020 chenzhizs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
